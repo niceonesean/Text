f278bc7acac55fdd4c36b44d7c99023de3b619b2
niceonesean <65555005+niceonesean@users.noreply.github.com>
2022-03-10 11:19:13 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 10 10:55:01 2022
+
+@author: User
+"""
+pip install TextBlob
+
+from flask import Flask, request, render_template
+app=Flask(__name__)
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method=="POST":
+        t=request.form.get("text")
+        print(t)
+        r=TextBlob(t).sentiment
+        return(render_template("index.html",results=r))
+    else:
+        return(render_template("index.html",results="2"))
+    
+if __name__=="__main__":
+    app.run()
